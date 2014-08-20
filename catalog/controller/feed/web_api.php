617579648ee3812f9ebd0f6f2c4eb8a7b2e55f3b
Aditya <t.a.shanker@gmail.com>
2014-08-20 15:29:26 +0530

    new api

--- catalog/controller/feed/web_api.php
+++ catalog/controller/feed/web_api.php
@@ -446,7 +446,8 @@ class ControllerFeedWebApi extends Controller {
     /*
      * Get the list of stores which are not mapped
      */
-    public function getUnmappedStoreList($mapped_store_list) {
+    public function getUnmappedStoreList() {
+        $mapped_store_list = $this->request->get['exclude_list'];
         $this->load->model("account/api");
         $unmapped_store_array = $this->model_account_api->getUnmappedStoreList($mapped_store_list);
         return json_encode($unmapped_store_array);
